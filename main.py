569e320260cdfa562a5bcccc6b38a544743ddceb
mahiuddun-dev <codermahiuddin@gmail.com>
2023-12-30 11:45:08 +0600

    Add days and month order status

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from typing import List
 import csv
+from datetime import datetime, date, timezone, timedelta
 
 DATABASE_URL = "sqlite:///./orders.db"
 engine = create_engine(DATABASE_URL)
@@ -90,8 +91,50 @@ async def upload_csv(file: UploadFile = File(...), db: Session = Depends(get_db)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
+def calculate_order_stats(orders):
+    stats_by_day = {}
+    stats_by_month = {}
+
+    for order in orders:
+        # By day
+        order_day_str = order.created_at
+        # 2023-11-20 00:52:28.64791+06 this is formetted datetime
+        order_day_str, tz_offset_str = order_day_str.rsplit('+', 1)
+        order_day = datetime.strptime(order_day_str, "%Y-%m-%d %H:%M:%S.%f")
+        tz_offset = timedelta(hours=int(tz_offset_str))
+        order_day = order_day.replace(tzinfo=timezone(tz_offset))
+        print(order_day)
+
+        if order_day not in stats_by_day:
+            stats_by_day[order_day] = {"total_orders": 0, "total_amount": 0.0}
+        stats_by_day[order_day]["total_orders"] += 1
+        stats_by_day[order_day]["total_amount"] += order.price
+
+        # By month
+        order_month = date(order_day.year, order_day.month, 1)
+        if order_month not in stats_by_month:
+            stats_by_month[order_month] = {"total_orders": 0, "total_amount": 0.0}
+        stats_by_month[order_month]["total_orders"] += 1
+        stats_by_month[order_month]["total_amount"] += order.price
+
+    return {"by_day": stats_by_day, "by_month": stats_by_month}
+
+# Route to get total count and amount of orders by days and months
 @app.get("/order-stats")
 def get_order_stats(db: Session = Depends(get_db)):
     orders = db.query(Order).all()
     stats = calculate_order_stats(orders)
+    return {"order_stats": stats}
+
+
+def calculate_order_stats_all(orders):
+    total_orders = len(orders)
+    total_amount = sum(order.price for order in orders)
+    return {"total_orders": total_orders, "total_amount": total_amount}
+
+@app.get("/order-stats-all")
+def get_order_stats(db: Session = Depends(get_db)):
+    orders = db.query(Order).all()
+    stats = calculate_order_stats_all(orders)
     return {"order_stats": stats}
